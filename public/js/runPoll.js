eb916ea3468cd4c0ece0e47d454d2c391c26b63d
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-02-12 12:42:19 -0600

    Working On runPoll Still.

--- public/js/runPoll.js
+++ public/js/runPoll.js
@@ -1,10 +1,10 @@
-const Poll = require('/models/poll.js');
-
-console.log(Poll);
-
 
 const runPoll = (Poll) => {
-    const timer = document.getElementById('timer');
+    const timer = document.querySelector('.timer');
+    const minutes = document.getElementById('minutes');
+    const seconds = document.getElementById('seconds');
+    const totalSeconds = 0;
+    const secondsElapsed = 0;
 
 
 };
