888efba675e4d791d0b7a263c7fed49fc723d281
David Rojo <rojo@rojorevolution.com>
2021-02-12 13:35:07 -0600

    set up variables and event listeners for homepage form, need to set up final function still

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
+
+    // variables for DOM elements
+    const signUpform = document.getElementById('signUpForm')
+    const orgInputValue = document.getElementById('org-input')
+    const emailInputValue = document.getElementById('email-input')
+    const passInputValue = document.getElementById('pass-input')
+
+    // Event listener for sign up form
+
+    signUpform.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let userData = {
+            org: orgInputValue.value.trim(),
+            email: emailInputValue.value.trim(),
+            password: passInputValue.value.trim(),
+        };
+
+        console.log(userData);
+
+        if (!userData.org || !userData.email || !userData.password) {
+            return;
+        }
+
+        signUpUser(userData.org, userData.email, userData.password);
+        orgInputValue.value = "";
+        emailInputValue.value = "";
+        passInputValue.value = "";
+
+    });
+
+    // Need to set up SignUpUser Function
+    signUpUser = (orgName, email, password) => {
+
+    }
 
 
-
-}
+});
