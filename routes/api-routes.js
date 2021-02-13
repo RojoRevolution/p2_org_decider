0ce17e0f026eca019e19d93c92e0f83468f2d945
Stephen Price <stephen_lprice@live.com>
2021-02-13 03:21:27 -0600

    added missing semi colons as well

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,24 +6,24 @@ module.exports = (app) => {
     // Post routes for logging in
     app.post("/api/login", passport.authenticate("local"), (req, res) => {
         console.log('///// API/LOGIN ////');
-        console.log(req.user)
+        console.log(req.user);
         res.json(req.user);
     });
     // Post routes for signing up
     app.post("/api/signup", (req, res) => {
-        console.log('//// API ROUTE ////')
-        console.log(req.body)
+        console.log('//// API ROUTE ////');
+        console.log(req.body);
         db.User.create({
             email: req.body.email,
             password: req.body.password,
-        })
+        });
         db.Org.create({
             name: req.body.org
         })
             .then(() => {
-                console.log('====================')
-                console.log('REDIRECTING TO LOGIN')
-                console.log('====================')
+                console.log('====================');
+                console.log('REDIRECTING TO LOGIN');
+                console.log('====================');
                 res.redirect(307, "/api/login");
             })
             .catch((err) => {
