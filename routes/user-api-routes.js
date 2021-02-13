d64e9323b42457b62b460a2eec799eb01f8877d4
Stephen Price <stephen_lprice@live.com>
2021-02-13 07:57:32 -0600

    added responses and error catching to GET

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -2,7 +2,18 @@ const db = require('../models');
 
 module.exports = (app) => {
   app.get('/api/users/', (req, res) => {
-    db.User.findAll().then((dbUser) => res.json(dbUser));
+    db.User.findAll().then((dbUser) => {
+      if (dbUser.length > 0) {
+        res.status(200).json(dbUser);
+      }
+      else {
+        res.status(404).json(dbUser);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   });
 
   app.post('/api/users/', (req, res) => {
