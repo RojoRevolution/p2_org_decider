d1ee3e6d6a89410c467c720fe7e7a703e3a3633f
Stephen Price <stephen_lprice@live.com>
2021-02-13 05:36:07 -0600

    signup flow checks for existing orgs, if none found creates a new one. Then assigns new user to the matching org

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,41 +13,42 @@ module.exports = (app) => {
     app.post("/api/signup", (req, res) => {
         console.log('//// API SIGNUP ////');
         console.log(req.body);
-        let org;
+        // check if Org already exists
         db.Org.findAll({
             where: {
                 name: req.body.org
             }
-        }).then((dbOrg) => {
+        })
+        .then((dbOrg) => {
             console.log('SIGNUP: dbOrg', dbOrg);
+            // If array contains results then create a User under that Org
             if (dbOrg.length > 0) {
                 console.log('200 Org:', req.body.org, 'Found');
                 res.status(200);
-                org = dbOrg;
+                // creates the new User
+                signUser(dbOrg, req.body, res);
             }
             else {
+                // If not matching Orgs are found, create one
                 console.log('404 Org:', req.body.org, 'Not Found');
                 res.status(404);
-                db.Org.create({ name: req.body.org });
+                db.Org.create({ name: req.body.org })
+                .then(() => {
+                    db.Org.findAll({
+                        where: {
+                            name: req.body.org
+                        }
+                    })
+                    .then((newOrg) => {
+                        // creates the new User after a new Org was created
+                        signUser(newOrg, req.body, res);
+                    });
+                });
             }
         })
         .catch((err) => {
             console.log(err);
             return res.status(500).json(err);
-        });
-        db.User.create({
-            email: req.body.email,
-            password: req.body.password,
-        })
-        .then(() => {
-            console.log('====================');
-            console.log('REDIRECTING TO LOGIN');
-            console.log('====================');
-            res.redirect(307, "/api/login");
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(401).json(err);
         });
     });
 
@@ -56,4 +57,28 @@ module.exports = (app) => {
         req.logout();
         res.redirect("/");
     });
-}
+};
+
+const signUser = (dbOrg, body, res) => {
+    // If the Org name matches then create a User
+    for (let i = 0; dbOrg.length > i; i++) {
+        if (dbOrg[i].name === body.org) {
+            db.User.create({
+                email: body.email,
+                password: body.password,
+                OrgId: dbOrg[i].id
+            })
+            .then(() => {
+                console.log('====================');
+                console.log('REDIRECTING TO LOGIN');
+                console.log('====================');
+                res.redirect(307, "/api/login");
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(401).json(err);
+            });
+            return;
+        }
+    }
+};
