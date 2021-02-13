b82b9414603d4fdd241642fb439c14fe0eb2a063
Stephen Price <stephen_lprice@live.com>
2021-02-13 03:51:33 -0600

    added catches to org-api-routes.js with response codes for debugging

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,13 +5,13 @@ var passport = require("../config/passport");
 module.exports = (app) => {
     // Post routes for logging in
     app.post("/api/login", passport.authenticate("local"), (req, res) => {
-        console.log('///// API/LOGIN ////');
+        console.log('///// API LOGIN ////');
         console.log(req.user);
         res.json(req.user);
     });
     // Post routes for signing up
     app.post("/api/signup", (req, res) => {
-        console.log('//// API ROUTE ////');
+        console.log('//// API SIGNUP ////');
         console.log(req.body);
         db.User.create({
             email: req.body.email,
@@ -20,15 +20,16 @@ module.exports = (app) => {
         db.Org.create({
             name: req.body.org
         })
-            .then(() => {
-                console.log('====================');
-                console.log('REDIRECTING TO LOGIN');
-                console.log('====================');
-                res.redirect(307, "/api/login");
-            })
-            .catch((err) => {
-                res.status(401).json(err);
-            });
+        .then(() => {
+            console.log('====================');
+            console.log('REDIRECTING TO LOGIN');
+            console.log('====================');
+            res.redirect(307, "/api/login");
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(401).json(err);
+        });
     });
 
     // Post route for logging out
--- routes/org-api-routes.js
+++ routes/org-api-routes.js
@@ -2,11 +2,19 @@ const db = require('../models');
 
 module.exports = (app) => {
   app.get('/api/orgs/', (req, res) => {
-    db.Org.findAll().then((dbOrg) => res.json(dbOrg));
+    db.Org.findAll().then((dbOrg) => res.status(200).json(dbOrg))
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json(err);
+    });
   });
 
   app.post('/api/orgs/', (req, res) => {
-    db.Org.create(req.body).then((dbOrg) => res.json(dbOrg));
+    db.Org.create(req.body).then((dbOrg) => res.status(201).json(dbOrg))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
   });
 
 };
