54d7dd0b25b2b284d381328ca9c1d3196236cdb1
Stephen Price <stephen_lprice@live.com>
2021-02-13 03:17:48 -0600

    cleaned up functions to use ES6

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,7 @@ var passport = require("../config/passport");
 
 module.exports = (app) => {
     // Post routes for logging in
-    app.post("/api/login", passport.authenticate("local"), function (req, res) {
+    app.post("/api/login", passport.authenticate("local"), (req, res) => {
         console.log('///// API/LOGIN ////');
         console.log(req.user)
         res.json(req.user);
@@ -20,19 +20,19 @@ module.exports = (app) => {
         db.Org.create({
             name: req.body.org
         })
-            .then(function () {
+            .then(() => {
                 console.log('====================')
                 console.log('REDIRECTING TO LOGIN')
                 console.log('====================')
                 res.redirect(307, "/api/login");
             })
-            .catch(function (err) {
+            .catch((err) => {
                 res.status(401).json(err);
             });
     });
 
     // Post route for logging out
-    app.get("/logout", function (req, res) {
+    app.get("/logout", (req, res) => {
         req.logout();
         res.redirect("/");
     });
