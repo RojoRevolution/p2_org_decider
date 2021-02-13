02c467d6700bb8136a48bc24efcd714538dbca92
Stephen Price <stephen_lprice@live.com>
2021-02-13 08:18:58 -0600

    added responses to idea GET

--- routes/idea-api-routes.js
+++ routes/idea-api-routes.js
@@ -2,7 +2,18 @@ const db = require('../models');
 
 module.exports = (app) => {
   app.get('/api/ideas/', (req, res) => {
-    db.Idea.findAll().then((dbIdea) => res.json(dbIdea));
+    db.Idea.findAll().then((dbIdea) => {
+      if (dbIdea.length > 0) {
+        res.status(200).json(dbIdea);
+      }
+      else {
+        res.status(404).json(dbIdea);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   });
 
   app.post('/api/ideas/', (req, res) => {
