4a16716a1fc7eb91a00e595a9f4e06fe671f0efd
David Rojo <rojo@rojorevolution.com>
2021-02-14 19:34:25 -0600

    scrapping the modal, will use a form within the left side bar. Added a close button after the input appears

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,20 +1,43 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const addNewBtn = document.getElementById('addBtn');
-    const categoryForm = document.getElementById('newCategoryForm');
-    const categoryInput = document.getElementById('categoryName');
+    const closeCatBtn = document.getElementById('closeBtn');
+    const newCatDiv = document.getElementById('newCatDiv')
+    const newCatForm = document.getElementById('addNewForm')
+    const newCatInput = document.getElementById('newCategory')
 
     addNewBtn.addEventListener('click', (event) => {
         event.preventDefault()
         console.log("click Add Button")
-
-        // categoryForm.addEventListener('submit', (event) => {
-        //     event.preventDefault();
-        //     console.log('Click Form Submit')
-        // });
-
+        newCatDiv.classList.remove('hide');
+        addNewBtn.classList.add('hide')
+        closeCatBtn.classList.remove('hide')
     });
 
+    closeCatBtn.addEventListener('click', (event) => {
+        event.preventDefault()
+        console.log("click Add Button")
+        newCatDiv.classList.add('hide');
+        closeCatBtn.classList.add('hide')
+        addNewBtn.classList.remove('hide')
+    });
+
+    newCatForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log(newCatInput.value);
+    })
+
+    // modal.addEventListener('submit', (event) => {
+    //     event.preventDefault();
+    //     console.log('Save Button Clicked')
+    // })
+
+    // const addNew = () => {
+    //     addNewModal.addEventListener('submit', (event) => {
+    //         event.preventDefault();
+    //         console.log('Click Form Submit')
+    //     });
+    // }
 
 
 })
